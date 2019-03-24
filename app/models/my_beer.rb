f77beebe1625d442e590dbf02195a1d5b815ed36
voycik <voycik@gmail.com>
2019-03-24 21:27:53 +0100

    Allow only integer to beer_id + spec

--- app/models/my_beer.rb
+++ app/models/my_beer.rb
@@ -1,4 +1,5 @@
 class MyBeer < ApplicationRecord
   belongs_to :user
   validates :user_id, uniqueness: { scope: :beer_id }
+  validates :beer_id, numericality: {only_integer: true}
 end
--- spec/models/my_beer_spec.rb
+++ spec/models/my_beer_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 RSpec.describe MyBeer, type: :model do
    it { should belong_to(:user) }
+   it { is_expected.to allow_value(11).for(:beer_id) }
+   it { is_expected.not_to allow_value("abc").for(:beer_id) }
 end
